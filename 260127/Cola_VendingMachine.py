a9a18d2b63bf9e2c76d9cc39eccd34e05f92a139
정명호 <jmh0856@gmail.com>
2026-01-28 11:01:32 +0900

    Update Cola_VendingMachine.py

--- 260127/Cola_VendingMachine.py
+++ 260127/Cola_VendingMachine.py
@@ -4,6 +4,6 @@ class Cola_VendingMachine(VendingMachine):
     def __init__(self):
         super().__init__(price = 800, machine_type = "콜라자판기")
     
-    def prepare_product(self):
+    def prepare_product(self): # 추상메소드를 구체적으로 구현(오버라이딩)
         print(f"[{self.machine_type}] 냉장 상태 확인 중...")
         print(f"[{self.machine_type}] 배출구로 이동 중...")
